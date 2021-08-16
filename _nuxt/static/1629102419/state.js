7df192f95b3a01c41b43d01598a23145eeeffeb0
Minato08 <jonaldmbalingit@gmail.com>
2021-08-16 16:27:49 +0800

    additional updates

---
+++ _nuxt/static/1629102419/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fiopokemon\u002F_nuxt\u002Fstatic\u002F1629102419",layout:"default",error:a,state:{pokemonNames:[],selectedPokemon:{},currentAbilityDesc:""},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fiopokemon\u002F",assetsPath:"\u002Fiopokemon\u002F_nuxt\u002F",cdnURL:a}}}}(null));
